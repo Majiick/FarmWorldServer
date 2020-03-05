73619e13b6f5f0bcf4049408232c7a24a9819603
Zan Smirnov <c15437072@mydit.ie>
2020-03-05 21:21:59 +0000

    Hello World

---
+++ Server/Program.cs
@@ -0,0 +1,40 @@
+﻿using LiteNetLib;
+using LiteNetLib.Utils;
+using System;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventBasedNetListener listener = new EventBasedNetListener();
+            NetManager server = new NetManager(listener);
+            server.Start(9050 /* port */);
+
+            listener.ConnectionRequestEvent += request =>
+            {
+                if (server.PeersCount < 10 /* max connections */)
+                    request.AcceptIfKey("SomeConnectionKey");
+                else
+                    request.Reject();
+            };
+
+            listener.PeerConnectedEvent += peer =>
+            {
+                Console.WriteLine("We got connection: {0}", peer.EndPoint); // Show peer ip
+                NetDataWriter writer = new NetDataWriter();                 // Create writer class
+                writer.Put("Hello client!");                                // Put some string
+                peer.Send(writer, DeliveryMethod.ReliableOrdered);             // Send with reliability
+            };
+
+            while (!Console.KeyAvailable)
+            {
+                server.PollEvents();
+                Thread.Sleep(15);
+            }
+            server.Stop();
+        }
+    }
+}
